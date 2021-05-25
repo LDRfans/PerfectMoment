7ba03da5d56aafd96ec1b8cd50fa7fb1ad792a31
renyj <673542024@qq.com>
2021-05-26 01:31:16 +0800

    testing some functions that make the selection grid

--- UIcodes/test.py
+++ UIcodes/test.py
@@ -2,26 +2,33 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
-
+import cv2
+img = cv2.imread("lena.tiff")
 class App(QWidget):
 
-    def __init__(self):
+    def __init__(self,m,n):
         super().__init__()
         self.title = 'PyQt5 button - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 320
-        self.height = 200
+        self.left = 900
+        self.top = 450
+        self.width = 512
+        self.height = 512
+        self.num_person = m
+        self.num_picture = n
         self.initUI()
     
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        button = QPushButton('PyQt5 button', self)
-        button.setToolTip('This is an example button')
-        button.move(100,70)
-        button.clicked.connect(self.on_click)
+        self.button = QPushButton('', self)
+        self.button.setToolTip('This is an example button')
+        self.button.setStyleSheet("background-image : url(lena.tiff);")
+        row,col,_ = img.shape
+        self.button.resize(row,col)
+
+        # self.button.move(100,70)
+        self.button.clicked.connect(self.on_click)
         
         self.show()
 
@@ -31,5 +38,6 @@ class App(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    m,n = 3,3
+    ex = App(m,n)
     sys.exit(app.exec_())
