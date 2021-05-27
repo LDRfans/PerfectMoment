2542f52b3b1c6cc0feefbbe6331b6c503e788726
gRox167 <525563615@qq.com>
2021-05-27 12:16:20 +0800

    fix index outflow

--- code/Extract.py
+++ code/Extract.py
@@ -32,6 +32,9 @@ def extract(img, dialation=0.25):
             pts.append(pos)
 
     # head bounding boxes
+    height,width = img_gray.shape
+    print(width,height)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     head_bounding_boxes = []
     for subject in rects:
         h = subject.height()
@@ -40,13 +43,16 @@ def extract(img, dialation=0.25):
         bottom = subject.bottom()
         left = subject.left()
         right = subject.right()
+        x=int(top-dialation*h) if int(top-dialation*h)>=0 else 0
+        y=int(left-dialation*w) if int(top-dialation*h)>=0 else 0
+        h=int(h+2*dialation*h) if int(top-dialation*h)+int(h+2*dialation*h)<height else height-int(h+2*dialation*h)
+        w=int(w+2*dialation*w) if int(top-dialation*w)+int(w+2*dialation*w)<width else width-int(w+2*dialation*w)
         head_bounding_boxes.append(
-            [int(left-dialation*w), int(top-dialation*h), int(w+2*dialation*w), int(h+2*dialation*h)])
+            [x, y, h,w])
     print(head_bounding_boxes)
 
     haar_upper_body_cascade = cv2.CascadeClassifier(
         "./haarcascade_upperbody.xml")
-    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     upper_body_bounding_boxes = haar_upper_body_cascade.detectMultiScale(
         img_gray,
         scaleFactor=1.1,
@@ -54,14 +60,13 @@ def extract(img, dialation=0.25):
         minSize=(25, 50),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-    width, height = img_gray.shape
     if len(upper_body_bounding_boxes)<len(head_bounding_boxes):
         upper_body_bounding_boxes=[]
         for subject in head_bounding_boxes:
-            left = subject[0]-0.25*subject[2]
-            top = subject[1]+subject[3]
+            left = subject[1]-0.25*subject[3]
+            top = subject[0]+subject[2]
             w = subject[2]*2
-            h = height - top-1
+            h = (height - top) if 
             upper_body_bounding_boxes.append([int(left),int(top),int(w),int(h)])
     print(upper_body_bounding_boxes)
     return pts, head_bounding_boxes, upper_body_bounding_boxes
@@ -72,7 +77,7 @@ if __name__ == '__main__':
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
     # cv2读取图像
-    img = cv2.imread("imgs/homo_test_2/photo2.jpg")
+    img = cv2.imread("imgs/homo_test_1/photo2.jpg")
     extract(img)
     # 取灰度
     # img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
