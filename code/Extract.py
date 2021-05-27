0cd027ab2e6a7a505a9a328a7e87f9e487f80982
gRox167 <525563615@qq.com>
2021-05-27 17:57:27 +0800

    body_mask

--- code/Extract.py
+++ code/Extract.py
@@ -3,11 +3,11 @@ import cv2
 import dlib
 
 
-def extract(img, dialation=0.25):
+def extract(img, dialation=[0.5,0.8,0.5,0.05]):
     '''
     Read an image and extract key points from it
     :param img: image to extract, type is ndarray
-           dialation: bounding box dialtion rate
+           dialation: bounding box dialtion rate left,up,right,down
     :return: A list of person info. person=>[head,body] posision
 
     '''
@@ -43,12 +43,12 @@ def extract(img, dialation=0.25):
         bottom = subject.bottom()
         left = subject.left()
         right = subject.right()
-        x=int(top-dialation*h) if int(top-dialation*h)>=0 else 0
-        y=int(left-dialation*w) if int(top-dialation*h)>=0 else 0
-        h=int(h+2*dialation*h) if int(top-dialation*h)+int(h+2*dialation*h)<height else height-int(h+2*dialation*h)
-        w=int(w+2*dialation*w) if int(top-dialation*w)+int(w+2*dialation*w)<width else width-int(w+2*dialation*w)
+        x=int(left-dialation[0]*w) if int(top-dialation[0]*h)>=0 else 0
+        y=int(top-dialation[1]*h) if int(top-dialation[1]*h)>=0 else 0
+        w=int(w+(dialation[0]+dialation[2])*w) if x+int(w+(dialation[0]+dialation[2])*w)<width else width-x
+        h=int(h+(dialation[1]+dialation[3])*h) if y+int(h+(dialation[1]+dialation[3])*h)<height else height-y
         head_bounding_boxes.append(
-            [x, y, h,w])
+            [x, y, w,h])
     print(head_bounding_boxes)
 
     haar_upper_body_cascade = cv2.CascadeClassifier(
@@ -63,11 +63,11 @@ def extract(img, dialation=0.25):
     if len(upper_body_bounding_boxes)<len(head_bounding_boxes):
         upper_body_bounding_boxes=[]
         for subject in head_bounding_boxes:
-            left = subject[1]-0.25*subject[3]
-            top = subject[0]+subject[2]
-            w = subject[2]*2
+            left = subject[0]-0.25*subject[2]
+            top = subject[1]+subject[3]
+            w = subject[2]*1.5 if left+w<width-1 else width
             h = height - top
-            upper_body_bounding_boxes.append([int(left),int(top),int(h),int(w)])
+            upper_body_bounding_boxes.append([int(left),int(top),int(w),int(h)])
     print(upper_body_bounding_boxes)
 
     data = [head_bounding_boxes, upper_body_bounding_boxes]
@@ -84,7 +84,17 @@ if __name__ == '__main__':
 
     # cv2读取图像
     img = cv2.imread("imgs/homo_test_1/photo1.jpg")
-    extract(img)
+    subject_info = extract(img)
+    for idx,subject in enumerate(subject_info):
+        head_bb = subject[0]
+        body_bb = subject[1]
+        cv2.rectangle(img, (head_bb[0],head_bb[1]), (head_bb[0]+head_bb[2], head_bb[1]+head_bb[3]), (0, 255, 0), 2)
+        cv2.rectangle(img, (body_bb[0],body_bb[1]), (body_bb[0]+body_bb[2], body_bb[1]+body_bb[3]), (0, 255, 0), 2)
+        print(head_bb,body_bb)
+    print(subject_info.shape)
+    # cv2.circle(img, pos, 3, color=(0, 255, 0))
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
     # 取灰度
     # img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
